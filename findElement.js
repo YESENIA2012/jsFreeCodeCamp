85f5b43e48ffe3a59628bdd4b0a8eb0b10b66254
YESENIA2012 <gyesenia173@gmail.com>
2022-04-18 09:20:02 -0500

    Added the statement of the exercise

--- findElement.js
+++ findElement.js
@@ -1,3 +1,7 @@
+/*Create a function that looks through an array arr and returns 
+the first element in it that passes a 'truth test'. This means 
+that given an element x, the 'truth test' is passed if func(x) is true. 
+If no element passes the test, return undefined. */
 function findElement(arr, func) {
   var num = 0;
   for (let counter = 0; counter < arr.length; counter++) {
