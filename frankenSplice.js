4bda3826cc503f5aab82842349a2d0316bcf047c
YESENIA2012 <gyesenia173@gmail.com>
2022-04-18 10:40:44 -0500

    Added the statement of the exercise

--- frankenSplice.js
+++ frankenSplice.js
@@ -1,3 +1,7 @@
+/*You are given two arrays and an index.
+Copy each element of the first array into the second array, in order.
+Begin inserting elements at index n of the second array.
+Return the resulting array. The input arrays should remain the same after the function runs. */
 function frankenSplice(arr1, arr2, n) {
   let copiaArray1 = arr1.map((number) => {
     return number;
