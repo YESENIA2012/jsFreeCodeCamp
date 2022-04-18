1b540e714234c9dc63716a2d26ef05e0c7c68e94
YESENIA2012 <gyesenia173@gmail.com>
2022-04-18 09:09:48 -0500

    Added the statement of the exercise

--- bouncer.js
+++ bouncer.js
@@ -1,3 +1,6 @@
+/*Remove all falsy values from an array. 
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.*/
+
 function bouncer(arr) {
   let newArray = [];
   for (let counter = 0; counter < arr.length; counter++) {
